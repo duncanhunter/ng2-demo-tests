25769ed6a074e952e9c173f1173365c65b784cc3
Duncan Hunter <duncan@duncanhunter.com.au>
2016-12-09 18:44:28 +1000

    update spec

--- src/app/purchase.spec.ts
+++ src/app/purchase.spec.ts
@@ -57,101 +57,12 @@ describe('Component: HeroDetail', () => {
       expect(purchaseButton).toBeTruthy();
     });
 
-     it('should submit a purchse when form is valid', () => {
+    it('should submit a purchse when form is valid', () => {
       this.purchase = ''
       fixture.detectChanges();
       const purchaseButton = fixture.debugElement.query(By.css('button[disabled="false"]'));
       // expect(purchaseButton).toBeTruthy();
     });
 
-  
-
-    //   it('should emit close without saved purchase when goBack is called directly', () => {
-    //     spyOn(appComponent.close, 'emit');
-    //     spyOn(window.history, 'back');
-
-    //     appComponent.goBack();
-
-    //     expect(appComponent.close.emit).toHaveBeenCalled();
-    //     expect(appComponent.close.emit).toHaveBeenCalledTimes(1);
-    //     expect(window.history.back).not.toHaveBeenCalled();
-    //   });
-
-    //   it('should attempt to save the purchase when save is called and navigate to purchase when save successful', async(() => {
-    //     appComponent.purchase = MockPurchase;
-    //     spyOn(appComponent, 'goBack');
-    //     spyOn(purchaseService, 'save').and.callFake(() => {
-    //       return Promise.resolve(MockPurchase);
-    //     });
-
-    //     appComponent.save().then(() => {
-    //       expect(purchaseService.save).toHaveBeenCalled();
-    //       expect(purchaseService.save).toHaveBeenCalledTimes(1);
-    //       expect(purchaseService.save).toHaveBeenCalledWith(MockPurchase);
-    //       expect(appComponent.purchase).toEqual(MockPurchase);
-    //       expect(appComponent.goBack).toHaveBeenCalled();
-    //       expect(appComponent.goBack).toHaveBeenCalledTimes(1);
-    //       expect(appComponent.goBack).toHaveBeenCalledWith(MockPurchase);
-    //       // done();
-    //     });
-    //   }));
-
-    // it('should attempt to save the purchase when save is called and display error when save errors', (done) => {
-    //   appComponent.purchase = MockPurchase;
-    //   const errorMsg = 'Some Error';
-    //   spyOn(appComponent, 'goBack');
-    //   spyOn(purchaseService, 'save').and.callFake(() => {
-    //     return Promise.reject(errorMsg);
-    //   });
-
-    //   appComponent.save().then(() => {
-    //     expect(purchaseService.save).toHaveBeenCalled();
-    //     expect(purchaseService.save).toHaveBeenCalledTimes(1);
-    //     expect(purchaseService.save).toHaveBeenCalledWith(MockPurchase);
-    //     expect(appComponent.purchase).toEqual(MockPurchase);
-    //     expect(appComponent.goBack).not.toHaveBeenCalled();
-    //     expect(appComponent.error).toBe(errorMsg);
-    //     done();
-    //   });
-    // });
-
-    // it('should emit close without saved purchase when goBack is called directly and go back when navigated is true', () => {
-    //   appComponent.navigated = true;
-    //   spyOn(appComponent.close, 'emit');
-    //   spyOn(window.history, 'back');
-
-    //   appComponent.goBack();
-
-    //   expect(appComponent.close.emit).toHaveBeenCalled();
-    //   expect(appComponent.close.emit).toHaveBeenCalledTimes(1);
-    //   expect(window.history.back).toHaveBeenCalled();
-    //   expect(window.history.back).toHaveBeenCalledTimes(1);
-    // });
-
-    // it('should create a new purchase and set navigated false when initialized without route id param', () => {
-    //   expect(appComponent.purchase).not.toBeDefined();
-
-    //   appComponent.ngOnInit();
-
-    //   expect(appComponent.navigated).toBeFalsy();
-    //   expect(appComponent.purchase).toEqual(new Hero());
-    // });
-
-    // it('should set the purchase to the purchase for the id and set navigated false when initialized with route id param', () => {
-    //   expect(appComponent.purchase).not.toBeDefined();
-    //   activatedRoute.params = Observable.of({ 'id': 1 });
-    //   spyOn(purchaseService, 'getHero').and.callFake(() => {
-    //     return {
-    //       then: function (callback) {
-    //         return callback(MockPurchase);
-    //       }
-    //     };
-    //   });
-
-    //   appComponent.ngOnInit();
-
-    //   expect(appComponent.navigated).toBeTruthy();
-    //   expect(appComponent.purchase).toEqual(MockPurchase);
-    // });
   });
 });
